4aff03679d28b00e4b050106749f82ffed661ad9
David Fateh <davidfateh@gmail.com>
2020-03-17 14:22:57 +0100

    checkpoint-1

--- src/index.tsx
+++ src/index.tsx
@@ -2,27 +2,10 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import {
   init,
-  FieldExtensionSDK,
-  AppExtensionSDK,
-  locations,
-  DialogExtensionSDK
 } from 'contentful-ui-extensions-sdk';
-import Config from './Config';
-import FieldEditor from './FieldEditor';
-import Dialog from './Dialog';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import './index.scss';
 
-function renderAtRoot(component: React.ReactElement) {
-  return render(component, document.getElementById('root'));
-}
-
-init(sdk => {
-  if (sdk.location.is(locations.LOCATION_APP_CONFIG)) {
-    renderAtRoot(<Config sdk={sdk as AppExtensionSDK} />);
-  } else if (sdk.location.is(locations.LOCATION_ENTRY_FIELD)) {
-    renderAtRoot(<FieldEditor sdk={sdk as FieldExtensionSDK} />);
-  } else if (sdk.location.is(locations.LOCATION_DIALOG)) {
-    renderAtRoot(<Dialog sdk={sdk as DialogExtensionSDK} />);
-  }
+init(() => {
+  render(<div>Hello, Unsplash App!</div>, document.getElementById('root'));
 });
