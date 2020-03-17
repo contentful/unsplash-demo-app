fcfa8faf5cd5b7dfae968cc92dbc2e748236426a
David Fateh <davidfateh@gmail.com>
2020-03-17 14:11:27 +0100

    removing hot reloading comment

--- src/index.tsx
+++ src/index.tsx
@@ -26,11 +26,3 @@ init(sdk => {
     renderAtRoot(<Dialog sdk={sdk as DialogExtensionSDK} />);
   }
 });
-
-/**
- * By default, iframe of the extension is fully reloaded on every save of a source file.
- * If you want to use HMR (hot module reload) instead of full reload, uncomment the following lines
- */
-// if (module.hot) {
-//   module.hot.accept();
-// }
